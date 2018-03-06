015421c96235121bd24eda4c3b9909f7bbc4dbdd
Bhupender Singh <bhupenders225@gmail.com>
2018-03-06 13:31:56 +0530

    updating last things

--- script.js
+++ script.js
@@ -31,3 +31,28 @@ var slider = function () {
     }
 }
 window.setInterval(slider, 4000);
+
+
+/*for nav*/
+var nav_button = document.querySelector(".navigation_button");
+var nav_background = document.querySelector(".navigation_background");
+var nav_nav = document.querySelector(".navigation_nav");
+var nav_item = document.querySelector(".navigation_list");
+var nav_span = document.querySelector(".navigation_icon");
+var nav_btn = document.querySelector(".navigation_button");
+nav_button.addEventListener("click", function () {
+
+    nav_background.classList.toggle("nav_backgroundToggle");
+    nav_nav.classList.toggle("nav_nav");
+
+});
+
+nav_item.addEventListener("click", function () {
+    nav_background.classList.remove("nav_backgroundToggle");
+    nav_nav.classList.remove("nav_nav");
+    nav_span.classList.toggle("icon1");
+});
+
+nav_btn.addEventListener("click", function () {
+    nav_span.classList.toggle("icon1");
+});
